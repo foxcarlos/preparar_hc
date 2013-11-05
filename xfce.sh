4ef0f273e6292c46d712241e9ab2138cf29dadad
FoxCarlos Trabajo <foxcarlos@gmail.com>
2013-11-05 09:49:30 -0430

    new file:   xfce.sh

---
+++ xfce.sh
@@ -0,0 +1,30 @@
+#entorno
+aptitude install xfce4 xfce4-goodies 
+
+#Navegador
+aptitude install iceweasel iceweasel-l10n-es-ar flashplugin-nonfree
+
+#Herramientas
+aptitude install gcalctool  clementine vlc smplayer smbclient cifs-utils git htop libespeak-dev espeak htop gimp file-roller vim-gnome vim-python mbrola mbrola-vz1 libreoffice terminator gnome-photo-printer gconf-editor 
+
+#Virtualizador
+aptitude install virtualbox virtualbox-guest-additions-iso 
+
+#desarrollo
+#postgres y mysql
+aptitude install pgadmin3 emma
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+#python
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Editor
+wget -c http://bitbucket.org/iep-project/iep/downloads/iep-3.3.linux32.tar.gz
+
+#modulos
+aptitude install python-paramiko python-setuptools python-xlwt python-psycopg2 fping python-matplotlib python-wxgtk2.8 python-pip python-daemon python-dbf python-tweepy libzmq-dev python-zmq python-pygame python-psutil
+
+easy_install pyttsx
+easy_install MplayerCtrl
+easy_install pydicom
+
